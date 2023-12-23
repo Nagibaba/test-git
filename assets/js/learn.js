5fa88a33e66328662c7eeb6e08764e778dce74a8
nagibaba <bnagiyev@gmail.com>
2023-12-23 14:31:46 +0400

    Last commit

--- assets/js/learn.js
+++ assets/js/learn.js
@@ -1,3 +1,16 @@
-// O(log n)
+// Anagram
 
-// dsfasd
+// "silent" = "listen"
+// {s: 1} {a}
+
+const isAnagram = (str1, str2) => {
+	str1Sorted = str1.split('').sort().join('')
+	str2Sorted = str2.split('').sort().join('')
+	return str1Sorted === str2Sorted
+	
+	
+}
+
+const str1 = 'silent'
+const str2 = 'listen'
+console.log(isAnagram(str1, str2))
